8df588361530a2d136fdc69c92c306d356190c93
Jimin-park1013 <as65253211@gmail.com>
2025-06-20 02:00:48 +0800

    06197

--- src/app/fb0529/page.js
+++ src/app/fb0529/page.js
@@ -1,30 +1,26 @@
 "use client"
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue, set, push } from "firebase/database";
 import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
 import { useEffect } from "react";
 
-export default function fb0529() {
+
+export default function FB0529() {
 
   // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDBJ8mxYxEh63jdaMzpDv1RL30GkKUu5nM",
-  authDomain: "nccu-113-2-yzc.firebaseapp.com",
-  projectId: "nccu-113-2-yzc",
-  storageBucket: "nccu-113-2-yzc.firebasestorage.app",
-  messagingSenderId: "889340655222",
-  appId: "1:889340655222:web:59dc9855c33dbed855d157",
-  measurementId: "G-EXCRBHRDZV",
-  databaseURL: "https://nccu-113-2-yzc-default-rtdb.firebaseio.com/"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyDBJ8mxYxEh63jdaMzpDv1RL30GkKUu5nM",
+    authDomain: "nccu-113-2-yzc.firebaseapp.com",
+    projectId: "nccu-113-2-yzc",
+    storageBucket: "nccu-113-2-yzc.firebasestorage.app",
+    messagingSenderId: "889340655222",
+    appId: "1:889340655222:web:59dc9855c33dbed855d157",
+    measurementId: "G-EXCRBHRDZV",
+    databaseURL: "https://nccu-113-2-yzc-default-rtdb.firebaseio.com/"  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const database = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig, "fb0529");
+  const database = getDatabase(app);
   const dbRef = ref(database, "/");
 
   const auth = getAuth();
@@ -40,7 +36,7 @@ const database = getDatabase(app);
     const userRef = ref(database, "/accounts/0000001/");
     
     set(userRef, {
-      name: "yzc",
+      name: "GUGU",
       points: 200
     });
 
@@ -62,12 +58,33 @@ const database = getDatabase(app);
   const login = ()=> {
 
     signInWithPopup(auth, provider).then((result)=>{
+      console.log(result);
+      console.log(result.user.uid);
       console.log(result.user.displayName);
+
+      const uid = result.user.uid;
+      const name = result.user.displayName;
+
+
+      const accountRef = ref(database, "/accounts/" + uid);
+      console.log(accountRef); 
+      
+      if(!accountRef){
+        //沒有此帳號，建立一個
+        console.log("enter");
+
+        push(accountRef, {
+          name: name,
+          type: "User",
+          point: "10",
+          uid: uid
+        });
+
+      }
+
     });
 
   }
-
-
 
   return (
     <>
@@ -77,6 +94,3 @@ const database = getDatabase(app);
     </>
   );
 }
-
-
- 
